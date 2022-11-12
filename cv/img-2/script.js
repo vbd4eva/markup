3d4ee80f52d7bb04fd9b6f82298edddbbe528691
Denis Onyshchuk <o.82.denis@gmail.com>
2022-11-12 22:43:21 +0200

    markup from img cv

---
+++ cv/img-2/script.js
@@ -0,0 +1,21 @@
+const scaleSorceRef = document.querySelector('.js__dev-scene-page');
+const opacitySorceceRef = document.querySelector('.js__dev-scene-image');
+const controlPanel = document.querySelector('.js__dev-scene-page-control');
+// console.log(scaleDivREf.style);
+controlPanel.addEventListener('click', controlClickHandler);
+
+
+const actionsData = {
+    opacity: { ref: opacitySorceceRef, cssRule: 'opacity', value: (val)=>val},
+    scale: {ref:scaleSorceRef, cssRule: 'transform', value: (val)=>`scale(${val})`}
+};
+
+function controlClickHandler(e) {
+    const {target} =  e ;
+    let action = target.classList?.value;
+    let param = target.dataset?.[action];
+    if (!param || !actionsData[action] ) return;
+
+    const { ref, cssRule, value } = actionsData[action];
+    ref.style[cssRule] = value(param);
+}
