b2a70dc2539cf9f77184f21ae1c13394484f1afa
Denis Onyshchuk <o.82.denis@gmail.com>
2022-12-19 20:07:20 +0200

    makes mouse effect

--- landing/elxyr/js/cursor-follow-effect.js
+++ landing/elxyr/js/cursor-follow-effect.js
@@ -1,78 +1,103 @@
-const domElement = {
-	check() { return ['create', 'addClassList', 'get'].every(i => this[i]) },
-	create: el => document.createElement(el || 'div'),
-	get(options) {
-		const el = this.create(options?.elem);
-		if (!options?.class?.length) return el;
-		this.addClassList.call(el, options.class);
-		return el;
+const domElementInterface = {
+	check() {
+		return ['create', 'addClassList', 'get', 'init'].every(i => this[i]);
 	},
+
+	create: el => document.createElement(el || 'div'),
+
+	get({ elem, classList, amount = 1 }) {
+		const elArr = new Array(1 * amount).fill().map(i => this.create(elem));
+		if (classList?.length) elArr.forEach(el => this.addClassList.call(el, classList));
+		return elArr;
+	},
+
+	init(elemOptions = {}, returnElements = false) {
+		if (!this?.check?.()) return;
+		elemOptions.ref = this.get(elemOptions);
+		return returnElements ? elemOptions.ref : true;
+	},
+
 	addClassList(classArr = []) {
 		if (!this?.classList || !classArr.length) return;
-		this.classList.add(classArr)
+		this.classList.add(classArr);
 	},
-}
+};
 
-const elementsToInsert = [];
+const initialHtmlElements = [];
+
+const effect = {
+	use: true,
+	// elem: 'div',
+	classList: ['cursor__follower'],
+	amount: 3,
+};
 
 const cursor = {
 	use: true,
-	class: ['cursor'],
-	init() {
-		if (!this.use || !domElement?.check?.()) return;
-		this.ref = domElement.get(this);
-		// return this?.ref?.nodeType && this.ref;
-		return Boolean(this?.ref?.nodeType);
-	}
+	// elem: 'div',
+	classList: ['cursor'],
+	// amount: 1,
+
 };
-// if (cursor.use) {
-// 	cursor.ref = getElement.call(cursor);
-// 	elementsToInsert = elementsToInsert ? elementsToInsert.after(cursor.ref) : cursor.ref;
-// };
+effect.use &&
+	domElementInterface.init(effect) &&
+	initialHtmlElements.push(...effect.ref);
 
-// cursor.init(); - return cursor Element
+cursor.use &&
+	domElementInterface.init(cursor) &&
+	initialHtmlElements.push(...cursor.ref);
 
 
-cursor.init() && elementsToInsert.push(cursor.ref)
+// const ecu = initialHtmlElements.reduce((acc, elem, index) => {
+// 	if (!acc) return elem;
+// 	console.log('acc', acc);
+// 	acc.after(elem);
+// 	console.log('acc', acc);
 
+// 	return acc;
+// })
 console.log(cursor.ref);
 
 
+// let value = arr.reduce(function (accumulator, item, index, array) {
+// 	// ...
+// }, [initial]);
+
 let clientX = -100;
 let clientY = -100;
-// const cursor = document.querySelector(".js__cursor");
-const cursorEffect = document.querySelector(".js__cursor-effect");
-const cursorDiv = cursorEffect.querySelectorAll(".js__cursor-effect-unit");
+const cursorRef = cursor.ref[0];
+const followersRef = effect.ref;
 const initCursor = () => {
-	document.body.before(cursor.ref);
-	// document.body.before(elementsToInsert);
-	document.addEventListener("mousemove", (e) => {
-		cursor.ref.style.top = e.pageY + "px";
-		cursor.ref.style.left = e.pageX + "px";
+	document.body.prepend(...initialHtmlElements);
+	document.addEventListener('mousemove', e => {
+
+		cursorRef.style.top = e.pageY + 'px';
+		cursorRef.style.left = e.pageX + 'px';
+
 		clientX = e.clientX;
 		clientY = e.clientY;
-
 	});
 
-	gsap.set(cursorDiv, {
+	gsap.set(followersRef, {
 		x: clientX,
-		y: clientY
+		y: clientY,
 	});
 
 	const render = () => {
-		gsap.to(cursorDiv, {
-			duration: .5,
+		gsap.to(followersRef, {
+			duration: 0.5,
 			x: clientX,
 			y: clientY,
 			stagger: {
-				amount: .5,
+				amount: 0.5,
 				from: 0,
-				ease: 'none'
-			}
+				ease: 'none',
+			},
 		});
 		requestAnimationFrame(render);
-	}
+	};
 	requestAnimationFrame(render);
-}
+};
 
-(!elementsToInsert || initCursor()) && console.log(`cursor-follow-effect.js and didn't start.`);
+(!initialHtmlElements || initCursor()) &&
+	console.log(`cursor-follow-effect.js and didn't start.`);
